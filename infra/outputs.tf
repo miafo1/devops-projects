88ab478559cfbda8f17160b755676aa6ad179991
miafo1 <miafoarnauld@gmail.com>
2025-12-15 08:23:03 -0800

    Acc: Add ECR repository URLs to Terraform outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,11 @@ output "alb_dns_name" {
 output "db_endpoint" {
   value = aws_db_instance.default.endpoint
 }
+
+output "ecr_repository_backend" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "ecr_repository_frontend" {
+  value = aws_ecr_repository.frontend.repository_url
+}
